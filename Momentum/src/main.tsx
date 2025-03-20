4750222c8a70841a0d19b4fc9d0dcc9b44322017
Otari Javakhadze <nikaotiko@gmail.com>
2025-03-20 20:38:30 +0400

    removed unnecessary imports from main.tsx

--- Momentum/src/main.tsx
+++ Momentum/src/main.tsx
@@ -3,13 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
-import {
-  useQuery,
-  useMutation,
-  useQueryClient,
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
